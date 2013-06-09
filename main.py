caeba021defcefefab67b359f3d5868132a2c222
Constantine Fedenko <cfedenko@gmail.com>
2013-06-10 02:13:07 +0300

    moving ball by path

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import itertools
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.graphics import Color, Line, Ellipse
@@ -9,6 +10,8 @@ from kivy.properties import (
     ObjectProperty
 )
 
+def to_points(tuples):
+    return [element for tupl in tuples for element in tupl]
 
 class Ball(Widget):
     velocity_x = NumericProperty(0)
@@ -16,31 +19,50 @@ class Ball(Widget):
     velocity = ReferenceListProperty(velocity_x, velocity_y)
 
     def move(self):
-        self.velocity = Vector(*self.velocity).rotate(1)
-        self.pos = Vector(*self.velocity) + self.pos
+        self.center = Vector(*self.velocity) + self.center
 
 
 class MainFrame(Widget):
     ball = ObjectProperty(None)
 
-    def serve_ball(self, vel=(2, 0)):
-        self.ball.center = self.center
-        self.ball.velocity = vel
+    def __init__(self, *args, **kw):
+        self.star7_3 = list(self.get_star(7, 3, 125, 125, 125))
+        self.star12_5 = list(self.get_star(12, 5, 500, 125, 125))
+        self.star7_3_points = to_points(self.star7_3)
+        self.star12_5_points = to_points(self.star12_5)
+        self.ball_path = itertools.cycle(self.star12_5[:-1])
+        super(MainFrame, self).__init__(*args, **kw)
+
+    def serve_ball(self):
+        self.start = self.ball_path.next()
+        self.finish = self.ball_path.next()
+        self.ball.center = self.start
+        v = Vector(self.start) - Vector(self.finish)
+        v = v.normalize() * -1
+        self.ball.velocity = v
 
     def update(self, dt):
         self.ball.move()
+        distance = Vector(self.ball.center).distance(self.finish)
+        if distance < 1:
+            self.start = self.finish
+            self.finish = self.ball_path.next()
+            v = Vector(self.start) - Vector(self.finish)
+            v = v.normalize() * -1
+            self.ball.velocity = v
 
     @staticmethod
-    def star(vertices, density, center_x, center_y, radius, angle=None):
+    def get_star(vertices, density, center_x, center_y, radius, angle=None):
+        """
+            Returns coordinates of star poligon:
+            (x1, y1), (x1, y2), ...
+        """
         v = Vector(0, radius)
         if angle:
             v = v.rotate(angle)
-        points = []
         for i in xrange(vertices+1):
             v = v.rotate(float(360)/vertices * density)
-            points.append(v.x + center_x)
-            points.append(v.y + center_y)
-        return points
+            yield v.x + center_x, v.y + center_y
 
 
 class StarsApp(App):
@@ -48,7 +70,7 @@ class StarsApp(App):
     def build(self):
         main_frame = MainFrame()
         main_frame.serve_ball()
-        Clock.schedule_interval(main_frame.update, 1.0 / 24)
+        Clock.schedule_interval(main_frame.update, 1.0 / 60)
         return main_frame
 
 if __name__ == '__main__':
